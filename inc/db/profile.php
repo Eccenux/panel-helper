f7f5f7f322b6f553e67d1dc8c38e011ea3b20eb7
Eccenux <eccenux+git@gmail.com>
2014-03-01 03:46:02 +0100

    [X]Osobna klasa na profile oraz testy getów i setów...

---
+++ inc/db/profile.php
@@ -0,0 +1,35 @@
+<?php
+$currentRoot = dirname(__FILE__);
+require_once $currentRoot.'/dbbase.class.php';
+
+/**
+ * Profile class
+ *
+ * @note Connection need to be established before methods of this class are used.
+ */
+class dbProfile extends dbBaseClass
+{
+	/**
+	 * @see dbBaseClass
+	 * @var string
+	 */
+	protected $pv_tableName = 'profile';
+
+	/**
+	 * @see dbBaseClass
+	 * @var array
+	 */
+	protected $pv_aliasNames2colNames = array (
+		'id' => 'id',
+		'dt' => 'dt',
+		
+		'ulica' => 'ulica',
+		'dzielnica' => 'dzielnica',
+		'plec' => 'plec',
+		'rok' => 'rok',
+		'wyksztalcenie' => 'wyksztalcenie',
+		'dzieci' => 'dzieci',
+	);
+}
+
+?>
--- test.php
+++ test.php
@@ -1,3 +1,22 @@
 <?php
+	require_once ('./inc/dbConnect.php');
+	require_once ('./inc/db/profile.php');
 
+	$dbProfile = new dbProfile();
+	$dbProfile->pf_getRecords($pv_array, array('dzielnica' => 'Wrzeszcz Dolny'));
+	/**
+	 * Get/set test
+	 *
+	var_export($pv_array[0]);
+	$id = $pv_array[0]['id'];
+	$prev = $pv_array[0]['plec'];
+	$pv_array[0]['plec'] = '123';
+	$dbProfile->pf_setRecords($pv_array[0], array('id' => $id));
+	$dbProfile->pf_getRecords($pv_array, array('id' => $id));
+	var_export($pv_array);
+	$pv_array['plec'] = $prev;
+	$dbProfile->pf_setRecords($pv_array, array('id' => $id));
+	$dbProfile->pf_getRecords($pv_array, array('id' => $id));
+	var_export($pv_array);
+	/**/
 ?>
