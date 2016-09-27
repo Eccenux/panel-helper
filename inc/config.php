36642d50d1bda257283adf1a1aaa9543ca504573
Eccenux <eccenux+git@gmail.com>
2016-09-27 11:11:17 +0200

    Stages info

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,11 @@
 <?php
 
+	/*
+		Stages:
+		# tests -- everything is accessible. Note! You are advised to anonimize data at this stage.
+		# draw -- searching and editing is accessible.
+		# results -- listing is unlocked, but editing is locked.
+	*/
 	define ('PANEL_STAGE', 'tests');
 
 	if ($_SERVER['HTTP_HOST'] != 'localhost') {
