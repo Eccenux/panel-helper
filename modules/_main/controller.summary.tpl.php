c37bc0d018b37aaa93024789f334688c38a785a9
Eccenux <eccenux+git@gmail.com>
2016-11-06 02:56:34 +0100

    actual stages info

--- modules/_main/controller.summary.tpl.php
+++ modules/_main/controller.summary.tpl.php
@@ -1,36 +1,26 @@
 <h2 id="system-wspomagaj-cy-losowanie">System wspomagający losowanie</h2>
 <p>Elementy wspomagania, czyli przebieg losowania z punktu widzenia kolejnych kroków wykonywanych przez operatora systemu wspomagania losowania - <em>Panel Helper</em>.</p>
-<h3 id="krok-1-przygotowanie-do-losowania">Krok 1. Przygotowanie do losowania</h3>
-<p>Przygotowanie umożliwia szybkie przeprowadzenie próbnego głosowania i dobranie odpowiednich parametrów, żeby wybór był w ogóle możliwy. Gdyby losowanie następowało ze wszystkich (większości) mieszkańców, to ten krok byłby zbędny.</p>
-<ol>
-<li>System prezentuje dwie wizualizacje (dla porównania):<ul>
-<li>Teoretycznego rozkładu (opracowanego na podstawie statystyk).</li>
-<li>Rzeczywistego rozkładu (z danych profilowych z ankiet).</li>
+<h3 id="krok-przygotowanie-do-losowania">Krok 1. Przygotowanie do losowania</h3>
+<p>Podczas przygotowania należy ustalić, które kryteria mogą zostać spełnione w trakcie losowania.</p>
+<p>Aby to ułatwić system prezentuje dwie wizualizacje (dla porównania):
+<ul>
+	<li>Teoretycznego rozkładu (opracowanego na podstawie statystyk).</li>
+	<li>Rzeczywistego rozkładu (z danych profilowych z ankiet).</li>
 </ul>
-</li>
-<li>Operator wprowadza maksymalne odchylenia dla każdego kryterium.</li>
-<li>Przykładowe kryteria wyboru odchyleń od teoretycznego rozkładu:<ul>
-<li>Jeśli zgłosi się mało osób z dziećmi, to dopuszczana jest możliwość rezygnacji z tego kryterium.</li>
-<li>Jeśli rozkład wiekowy zgłoszonych będzie znacząco inny niż teoretyczny, to dopuszczany będzie wybór osoby +/- 5 lat poza granice danej grupy wiekowej.</li>
-</ul>
-</li>
-<li>System wykonuje 3 losowania testowe, które mają wykazać, na ile sprawdzi się procedura przy zadanych odchyleniach.</li>
-<li>Operator decyduje czy przeprowadzić właściwe losowanie przy zadanych kryteriach.</li>
-</ol>
-<p>Jedynie punkt 1 z powyższych jest obecnie zrealizowany (ze względu małą liczbę zgłoszeń). Operator nie musi też z góry wprowadzać odchyleń.</p>
-<h3 id="krok-2-losowanie-grupy-g-wnej-i-zast-pczej">Krok 2. Losowanie grupy głównej i zastępczej</h3>
+<p>Operator decyduje jakie kryterie zostaną użyte przy losowaniu i tworzy profile statystyczne używane później przy losowaniu.</p>
+<h3 id="krok-losowanie-grupy-g-wnej-i-zast-pczej">Krok 2. Losowanie grupy głównej i zastępczej</h3>
+<p>Ten etap powienien być transmitowany na żywo, aby uwiarygodnić losowanie.</p>
 <ol>
-<li>Operator wprowadza profil jako kryteria wyszukiwania. Operator ma swobodę rezygnacji z dowolnych kryteriów (ignorowania ich).</li>
-<li>Wyświetlane są wszystkie dopasowane profile.</li>
-<li>Operator może zmienić kryteria lub ponownie wylosować profil w wypadku zbyt małej liczby osób do przeprowadzenia losowania.</li>
-<li>Operator przenosi wylosowaną osobę do grupy głównej.</li>
-<li>Z tej samej listy losowana jest przynajmniej 1 osoba do grupy zastępczej.</li>
-<li>Jeśli rodzaj miejsca zamieszkania jest wyczerpany, to pozostałe osoby można przenieść do grupy roboczej.</li>
+	<li>Operator wprowadza profil jako kryteria wyszukiwania. Operator ma swobodę rezygnacji z dowolnych kryteriów (ignorowania ich).</li>
+	<li>Wyświetlane są wszystkie dane dopasowane do wybranego profilu statystycznego.</li>
+	<li>Operator może zmienić kryteria lub ponownie wylosować profil w wypadku zbyt małej liczby osób do przeprowadzenia losowania.</li>
+	<li>Operator przenosi wylosowaną osobę do grupy głównej.</li>
+	<li>Z tej samej listy losowana jest przynajmniej 1 osoba do grupy zastępczej.</li>
 </ol>
 <p>Dane kontaktowe są widoczne dopiero na koniec i tylko dla operatora. Jedynie dana osoba może zweryfikować kiedy została wylosowana (zna swój identyfikator).</p>
-<h3 id="krok-3-losowanie-grupy-rezerwowej">Krok 3. Losowanie grupy rezerwowej</h3>
-<p>Z punktu widzenia systemu jedną różnicą jest kwestia rodzaju miejsca zamieszkania. Operator losuje i wpisuje profile z uwzględnieniem miesjsca zamieszkania. System umożliwia zatem także przefiltrowanie od razu do wybranego miejsca zamieszkania. </p>
-<h3 id="krok-4-wyniki">Krok 4. Wyniki</h3>
-<ol>
-<li>System jest przełączany w tryb wyników. Blokowana jest możliwość zmiany grupy i staje się możliwe wyświetlenie listy wylosowanych.</li>
-<li>Do publicznej wiadomości podawana jest lista imion i nazwisk posortowana alfabetycznie.</li></ol>
+<h3 id="krok-wyniki">Krok 3. Wyniki</h3>
+<p>Ten etap może być transmitowany na żywo jeśli losowane osoby wyraziły w ankietach zgodę na ujawnienie imienia i nazwiska.</p>
+<p>System jest przełączany w tryb wyników przez administratora na prośbę operatora.
+Blokowana jest przy tym możliwość zmiany grupy, a staje się możliwe wyświetlenie listy wylosowanych.</p>
+<p>Na listach domyślnie widoczna są tylko imioniona i nazwiska posortowane alfabetycznie.</p>
+<p>Jeśli ten etap jest transmitowany, to transmisję należy zakończyć przed odkryciem danych kontaktowych.</p>
