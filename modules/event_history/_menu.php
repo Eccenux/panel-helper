18452fb3e05c571b67d12cd601d588c9516c3b58
Eccenux <eccenux+git@gmail.com>
2017-03-16 12:39:56 +0100

    show list-server for admins

--- modules/event_history/_menu.php
+++ modules/event_history/_menu.php
@@ -2,7 +2,14 @@
 	/* @var $pv_menuItem MenuItem */
 	$pv_menuItem->title = 'Historia';
 	$pv_menuItem->order = 5;
-	//$pv_menuItem->users = AUTH_GROUP_OPS;
+	$pv_menuItem->users = AUTH_GROUP_OPS;
 	
 	$pv_menuItem->addSubItem('list','historia działań');
+
+	// Note! This is just a visibility thing, NOT actual authorization!
+	$users = explode(',', AUTH_GROUP_ADMIN);
+	if (in_array($userName, $users))
+	{
+		$pv_menuItem->addSubItem('list-server','zapisana historia');
+	}
 ?>
