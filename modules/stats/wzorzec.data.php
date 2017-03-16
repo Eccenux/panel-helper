f9713e94c64079062f9bfa8fc4ad1f73eb23758f
Eccenux <eccenux+git@gmail.com>
2017-03-16 05:42:12 +0100

    update stats based on more recent data https://www.facebook.com/photo.php?fbid=1478326518844893&set=a.186717838005774.47895.100000026771338&type=3&theater

--- modules/stats/wzorzec.data.php
+++ modules/stats/wzorzec.data.php
@@ -9,43 +9,43 @@ $pv_wzorzecData = array(
 	"wyksztalcenie" => array(
 		array (
 			"title" => "p",
-			"value" => reScaleData(20/40),
+			"value" => reScaleData(12.8/100),
 		),
 		array (
 			"title" => "ś",
-			"value" => reScaleData(13/40),
+			"value" => reScaleData(57.1/100),
 		),
 		array (
 			"title" => "w",
-			"value" => reScaleData(7/40),
+			"value" => reScaleData(30.1/100),
 		)
 	),
 	"plec" => array(
 		array (
 			"title" => "kobieta",
-			"value" => reScaleData(21/40),
+			"value" => reScaleData(54.4/100),
 		),
 		array (
 			"title" => "mężczyzna",
-			"value" => reScaleData(19/40),
+			"value" => reScaleData(45.6/100),
 		),
 	),
 	"wiek" => array(
 		array (
 			"title" => "18-24",
-			"value" => reScaleData(4/40),
+			"value" => reScaleData(6.5/100),
 		),
 		array (
 			"title" => "25-39",
-			"value" => reScaleData(12/40),
+			"value" => reScaleData(27.6/100),
 		),
 		array (
 			"title" => "40-64",
-			"value" => reScaleData(17/40),
+			"value" => reScaleData(39.9/100),
 		),
 		array (
 			"title" => "65+",
-			"value" => reScaleData(7/40),
+			"value" => reScaleData(26.0/100),
 		)
 	),
 );
