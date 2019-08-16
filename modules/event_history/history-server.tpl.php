4fb2fc04152b658faa0180909fa88d688536e8a9
Eccenux <eccenux+git@gmail.com>
2019-08-16 13:10:52 +0200

    show other history items then just last two

--- modules/event_history/history-server.tpl.php
+++ modules/event_history/history-server.tpl.php
@@ -4,9 +4,20 @@
 	ostatnia zmiana: <?=$tplData['history']['dt_change']?>.</p>
 <?php } ?>
 <div class="draw-history draw-history-full"></div>
-<p><input type="button" name="history-not-current" value="Pokaż nie-bieżącą"
-		  onclick="location.href = location.href.replace(/&uuid=[^&]*/, '') + '&amp;not_uuid=' + drawHistory.historyId" /></p>
+<p><input type="button" name="history-not-current" value="Pokaż nie-bieżącą" /></p>
 <script>
+// append currently displayed to the list of exception and reload
+var currentUuid = '<?=$tplData['history']['uuid']?>';
+$('[name="history-not-current"]').click(function(){
+	var previousExceptions = "";
+	var baseLocation = location.href.replace(/&uuid=[^&]*/, '').replace(/&not_uuid=([^&]*)/, function(a, previous) {
+		previousExceptions = previous;
+		return "";
+	});
+	location.href = baseLocation + '&not_uuid=' + (previousExceptions ? previousExceptions + "," : "") + currentUuid
+});
+
+// server history for JS
 window.serverHistoryData = [];
 <?php if (!empty($tplData['history'])) { ?>
 	window.serverHistoryData = JSON.parse(<?=json_encode($tplData['history']['history_data'])?>);
