a7b23db94fb990288a8f9157e55f3bc3b20fbb3f
Eccenux <eccenux+git@gmail.com>
2019-08-16 13:10:00 +0200

    show currently selected menu item

--- tpls/_menu.tpl.php
+++ tpls/_menu.tpl.php
@@ -3,13 +3,15 @@
 <? } else { ?>
 	<ul>
 		<? foreach ($pv_mainMenu as $m) { ?>
+			<? $isCurrentModule = ($pv_controller->moduleName === $m->moduleName) ?>
 			<? if (empty($m->submenu)) { ?>
-				<li><a href="<?=$m->url?>"><?=htmlspecialchars($m->title)?></a></li>
+				<li class="leaf <?=($isCurrentModule ? 'current' : '')?>"><a href="<?=$m->url?>"><?=htmlspecialchars($m->title)?></a></li>
 			<? } else { ?>
-				<li><a href="<?=$m->url?>"><?=htmlspecialchars($m->title)?></a>
+				<li class="branch <?=($isCurrentModule ? 'current' : '')?>"><a href="<?=$m->url?>"><?=htmlspecialchars($m->title)?></a>
 					<ul>
 						<? foreach ($m->submenu as $sm) { ?>
-							<li><a href="<?=$sm['url']?>"><?=htmlspecialchars($sm['title'])?></a></li>
+							<? $isCurrentAction = ($pv_controller->action === $sm['action']) ?>
+							<li class="leaf <?=($isCurrentAction ? 'current' : '')?>"><a href="<?=$sm['url']?>"><?=htmlspecialchars($sm['title'])?></a></li>
 						<? } ?>
 					</ul>
 				</li>
