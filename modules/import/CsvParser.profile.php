8f540744257250bf9d2bababc7df10e781221bc5
Eccenux <eccenux+git@gmail.com>
2020-03-18 10:30:52 +0100

    education parser

--- modules/import/CsvParser.profile.php
+++ modules/import/CsvParser.profile.php
@@ -20,3 +20,31 @@
 	$columnParsers['region'] = $columnParsers['group_name'] = function($name, $value){
 		return CsvParser::parseColumnRequired($name, $value);
 	};
+
+	// education check and extracting first letter
+	// wyższe -> w
+	$columnParsers['education'] = function($name, $value){
+		if (strlen($value) === 1) {
+			return CsvParser::parseColumnRequired($name, $value);
+		}
+		$start = substr($value, 0, 2);
+		$parsedValue = '';
+		switch ($start)
+		{
+			case 'po':
+			case 'wy':
+			case 'sr':
+				$parsedValue = substr($value, 0, 1);
+			break;
+			case 'śr':
+			case 'ś':
+				$parsedValue = 's';
+			break;
+		}
+		return array(
+			'state' => !empty($parsedValue) ? CsvRowState::OK : CsvRowState::INVALID,
+			'columns' => array(
+				$name => $parsedValue,
+			)
+		);
+	};
