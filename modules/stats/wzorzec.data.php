79a67e5bc2db87397fb86e239fdb08ba2a208f24
Eccenux <eccenux+git@gmail.com>
2020-03-18 11:55:47 +0100

    Wrocław generic data

--- modules/stats/wzorzec.data.php
+++ modules/stats/wzorzec.data.php
@@ -1,7 +1,7 @@
 <?php
 // rescale to expected total
 function reScaleData($fraction) {
-	$total = 56;
+	$total = 75;
 	return round($fraction * $total, 1);
 }
 
@@ -9,43 +9,61 @@ $pv_wzorzecData = array(
 	"wyksztalcenie" => array(
 		array (
 			"title" => "p",
-			"value" => reScaleData(12.8/100),
+			"value" => reScaleData(11.08/100),
 		),
 		array (
 			"title" => "ś",
-			"value" => reScaleData(57.1/100),
+			"value" => reScaleData(55.66/100),
 		),
 		array (
 			"title" => "w",
-			"value" => reScaleData(30.1/100),
+			"value" => reScaleData(33.27/100),
 		)
 	),
 	"plec" => array(
 		array (
 			"title" => "kobieta",
-			"value" => reScaleData(54.4/100),
+			"value" => reScaleData(55.08/100),
 		),
 		array (
 			"title" => "mężczyzna",
-			"value" => reScaleData(45.6/100),
+			"value" => reScaleData(44.92/100),
 		),
 	),
 	"wiek" => array(
 		array (
 			"title" => "18-24",
-			"value" => reScaleData(6.5/100),
+			"value" => reScaleData(6.00/100),
 		),
 		array (
 			"title" => "25-39",
-			"value" => reScaleData(27.6/100),
+			"value" => reScaleData(28.09/100),
 		),
 		array (
 			"title" => "40-64",
-			"value" => reScaleData(39.9/100),
+			"value" => reScaleData(39.11/100),
 		),
 		array (
 			"title" => "65+",
-			"value" => reScaleData(26.0/100),
+			"value" => reScaleData(26.81/100),
 		)
+	),
+	"transport" => array(
+		array (
+			"title" => "auto",
+			"value" => reScaleData(41.61/100),
+		),
+		array (
+			"title" => "zbiorkom",
+			"value" => reScaleData(27.74/100),
+		),
+		array (
+			"title" => "rower",
+			"value" => reScaleData(6.33/100),
+		),
+		array (
+			"title" => "pieszo",
+			"value" => reScaleData(24.32/100),
+		),
 	),
 );
